61663566d661e1afb3be479a7b53e55028c9a43b
MoeBagheri76-2 <125768162+MoeBagheri76-2@users.noreply.github.com>
2023-02-26 20:20:10 -0500

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 111, name: 'Moe Bagheri 1' },
+  { id: 222, name: 'Moe Bagheri 2' },
+  { id: 333, name: 'Moe Bagheri 3' },
+  { id: 444, name: 'Moe Bagheri 4' },
+  { id: 555, name: 'Moe Bagheri 5' },
+  { id: 666, name: 'Moe Bagheri 6' },
+  { id: 777, name: 'Moe Bagheri 7' },
+  { id: 888, name: 'Moe Bagheri 8' },
+  { id: 999, name: 'Moe Bagheri 9' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
